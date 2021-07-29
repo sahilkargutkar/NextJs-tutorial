f6013a7977a2a55323d140298a1311b0292f5759
sahilkargutkar <sahilkargutkar.sk@gmail.com>
2021-07-29 16:17:26 +0530

    added typescript file

---
+++ Typescript Learning/basics.ts
@@ -0,0 +1,11 @@
+function add(n1, n2) {
+  return n1 + n2;
+}
+
+const number1 = 5;
+const number2 = 2.8;
+
+const result = add(number1, number2);
+console.log(result);
+
+//use command tsc *filename*.ts to run the file
